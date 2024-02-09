160f2c377555a3e37cf43e4f0f9f3e8288474d41
Virajisesiri <viraji.sesiri@gmail.com>
2024-02-09 22:36:58 +0530

    Implement drag and drop

--- dist/index.8f0c9192.js
+++ dist/index.8f0c9192.js
@@ -2,6 +2,8 @@ const gameBoard = document.querySelector("#gameboard");
 const playerDisplay = document.querySelector("#player");
 const infoDisplay = document.querySelector("#info-display");
 const width = 8;
+let playerGo = "black";
+playerDisplay.textContent = "black";
 const startPieces = [
     rook,
     knight,
@@ -73,6 +75,7 @@ function createBoard() {
         const square = document.createElement("div");
         square.classList.add("square");
         square.innerHTML = startPiece;
+        square.firstChild && square.firstChild.setAttribute("draggable", true);
         square.setAttribute("square-id", i);
         // square.classList.add('beige')
         const row = Math.floor(i / width);
@@ -84,5 +87,47 @@ function createBoard() {
     });
 }
 createBoard();
+let startPositionId;
+let draggedElement;
+const allSquares = document.querySelectorAll(".square");
+allSquares.forEach((square)=>{
+    square.addEventListener("dragstart", dragStart);
+    square.addEventListener("dragover", dragOver);
+    square.addEventListener("drop", dragDrop);
+});
+function dragStart(e) {
+    startPositionId = e.target.parentNode.getAttribute("squre-id");
+    draggedElement = e.target;
+}
+function dragOver(e) {
+    e.preventDefault();
+}
+function dragDrop(e) {
+    e.stopPropagation();
+    const taken = e.target.classList.contains("piece");
+    // e.target.parentNode.append(draggedElement);
+    // e.target.remove();
+    //e.target.append(draggedElement);
+    changePlayer();
+}
+function changePlayer() {
+    if (playerGo === "black") {
+        reverseId();
+        playerGo = "white";
+        playerGo.textContent = "white";
+    } else {
+        revertIds();
+        playerGo = "black";
+        playerDisplay.textContent = "black";
+    }
+}
+function reverseId() {
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square, i)=>square.setAttribute("square-id", width * width - 1 - i));
+}
+function revertIds() {
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square, i)=>square.setAttribute("square-id", i));
+}
 
 //# sourceMappingURL=index.8f0c9192.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ const infoDisplay = document.querySelector("#info-display");
 
 const width = 8;
 
+let playerGo = 'black';
+playerDisplay.textContent = 'black';
+
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
@@ -21,6 +24,7 @@ function createBoard() {
         const square = document.createElement('div');
         square.classList.add('square');
         square.innerHTML = startPiece;
+        square.firstChild && square.firstChild.setAttribute('draggable', true);
         square.setAttribute('square-id', i);
        // square.classList.add('beige')
        const row = Math.floor(i / width);
@@ -38,3 +42,58 @@ function createBoard() {
 }
 
 createBoard()
+
+let startPositionId;
+let draggedElement;
+
+const allSquares = document.querySelectorAll(".square");
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart);
+    square.addEventListener('dragover', dragOver);
+    square.addEventListener('drop', dragDrop);
+
+});
+
+function dragStart(e){
+    startPositionId = e.target.parentNode.getAttribute('squre-id');
+    draggedElement = e.target;
+}
+
+function dragOver (e) {
+    e.preventDefault();
+}
+
+function dragDrop(e){
+    e.stopPropagation();
+
+    const taken = e.target.classList.contains('piece');
+
+    // e.target.parentNode.append(draggedElement);
+    // e.target.remove();
+    //e.target.append(draggedElement);
+    changePlayer()
+}
+
+function changePlayer() {
+    
+    if(playerGo === 'black'){
+        reverseId()
+        playerGo = "white";
+        playerGo.textContent = 'white';
+    }else {
+        revertIds()
+        playerGo = 'black';
+        playerDisplay.textContent = 'black';
+    }
+}
+
+function reverseId() {
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square, i) => square.setAttribute('square-id', (width * width - 1) - i)
+)}
+
+function revertIds() {
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square, i) => square.setAttribute('square-id', i));
+}
+
